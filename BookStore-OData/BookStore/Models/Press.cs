40a99669d5a8f48d5761c798c8aeec2b673b35f9
Hakan Sinanoglu <hknsnngl52@gmail.com>
2019-06-09 17:19:56 +0300

    Net Core 2.1 OData

---
+++ BookStore-OData/BookStore/Models/Press.cs
@@ -0,0 +1,17 @@
+﻿namespace BookStore.Models
+{
+    public class Press
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public enum Category
+    {
+        Book,
+        Magazine,
+        EBook
+    }
+}
---
+++ BookStore-OData/BookStore/Startup.cs
@@ -0,0 +1,52 @@
+﻿using BookStore.Models;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OData.Edm;
+
+namespace BookStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<BookStoreContext>(opt => opt.UseInMemoryDatabase("BookLists"));
+            services.AddOData();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(b =>
+            {
+                b.Select().Expand().Filter().OrderBy().MaxTop(100).Count();
+                b.MapODataServiceRoute("odata", "odata", GetEdmModel());
+            });
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Book>("Books");
+            builder.EntitySet<Press>("Presses");
+            return builder.GetEdmModel();
+        }
+    }
+}
